32c66e72b8478b5374ea2b286bce76b0eee62cc1
blueC4P <osamag4g@gmail.com>
2021-08-29 01:30:34 +0100

    create list of tasks with input

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 let mainCointainer = document.querySelector("#manipulate");
 
+let createNewList = () => {
+
+     let input=  document.getElementById('add-new-list');
+
+     let list = new List(input.value);
+     let listHTML = document.createElement("div");
+     listHTML.setAttribute('id', list.id)
+     listHTML.className = "card";
+     listHTML.textContent = list.title;
+     mainCointainer.appendChild(listHTML);
+     input.value = ''
+     console.log(input.value)
+
+}
+
 // Creating a column of tasks
 
-let column1 = new List("To do");
-let columnHTML = document.createElement("div");
-columnHTML.className = "card";
-columnHTML.textContent = column1.title;
-mainCointainer.appendChild(columnHTML);
+
 
 // Creating tasks
 
-let task1 = new Task("task1");
-let task2 = new Task("task2");
-let task3 = new Task("task3");
+// let task1 = new Task("task1");
+// let task2 = new Task("task2");
+// let task3 = new Task("task3");
 
-column1.addTask(task1);
-column1.addTask(task2);
-column1.addTask(task3);
+// column1.addTask(task1);
+// column1.addTask(task2);
+// column1.addTask(task3);
 
-let tasksHTML = document.createElement("ul");
-columnHTML.appendChild(tasksHTML);
+// let tasksHTML = document.createElement("ul");
+// columnHTML.appendChild(tasksHTML);
 
-column1.tasks.map((task) => {
-  let newTask = document.createElement("li");
-  newTask.textContent = task.title;
-  tasksHTML.appendChild(newTask);
-});
+// column1.tasks.map((task) => {
+//   let newTask = document.createElement("li");
+//   newTask.textContent = task.title;
+//   tasksHTML.appendChild(newTask);
+// });
 
 // Crear una nueva lista
 
@@ -35,14 +46,23 @@ addNewColumn.classList = "create-new-column";
 if(document.getElementsByClassName('card')){
       mainCointainer.appendChild(addNewColumn)
 }else{
+      document.getElementsByClassName('create-new-column').removeChild()
       mainCointainer.after(addNewColumn);
 }
 
 let newColumnForm = document.createElement("form");
+newColumnForm.setAttribute("id", "add-new-list-form");
 addNewColumn.appendChild(newColumnForm);
 
 let newColumnInput = document.createElement("input");
+
 newColumnForm.appendChild(newColumnInput);
 
-newColumnInput.setAttribute("id", "add-list");
-document.getElementById("add-list").placeholder = "+ Añada una lista";
+newColumnInput.setAttribute("id", "add-new-list");
+document.getElementById("add-new-list").placeholder = "+ Añada una lista";
+
+
+newColumnForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      createNewList()
+})
