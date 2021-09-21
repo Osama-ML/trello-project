a07da5f5e730a8eb8685406aea9e80b4fc619020
blueC4P <osamag4g@gmail.com>
2021-09-21 23:28:12 +0100

    trying to open dialog to edit tasks

--- view/tasksComponent.js
+++ view/tasksComponent.js
@@ -4,7 +4,11 @@ createTasksView = (columnId, tasks) => {
     let tasksList = document.createElement("ul");
     let taskHTML = document.createElement("li");
 
+    let taskDialog = document.createElement('dialog')
+    taskDialog.textContent = 'hi from dialog'
+
     tasksDiv.appendChild(tasksList);
+
 
     tasks.map((task) => {
         taskHTML.innerText = task.title;
@@ -12,4 +16,15 @@ createTasksView = (columnId, tasks) => {
     });
 
     tasksList.appendChild(taskHTML);
+    tasksList.appendChild(taskDialog);
+
+    taskHTML.addEventListener('click', () => {
+        console.log('hi')
+        taskDialog.showModal()
+    })
+    taskDialog.addEventListener('blur', () => {
+        console.log('hi blur')
+        taskDialog.close()
+
+    })
 };
